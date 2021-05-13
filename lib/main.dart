374a04d75babc1539134cb7cb146735fad29868e
dave <a@b.c.de>
2021-05-13 18:51:39 +0100

    2 page navigation working

--- lib/main.dart
+++ lib/main.dart
@@ -1,126 +1,147 @@
+/// Flutter code sample for DropdownButton
+
+// This sample shows a `DropdownButton` with a large arrow icon,
+// purple text style, and bold purple underline, whose value is one of "One",
+// "Two", "Free", or "Four".
+//
+// ![](https://flutter.github.io/assets-for-api-docs/assets/material/dropdown_button.png)
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(SheepSkin());
+  runApp(MaterialApp(
+    title: 'SheepSkin - Wallpaper Fluctuator',
+    home: OptionsRoute(),
+  ));
 }
 
-/// This is the main application widget.
-class SheepSkin extends StatelessWidget {
-  const SheepSkin({Key? key}) : super(key: key);
-
-  static const String _title = 'Flutter Code Sample';
-
+class OptionsRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
-        body: const Center(
-          child: SheepskinSettingsWidget(),
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('SheepSkin Settings'),
         ),
-      ),
-    );
+        body: Center(child: SheepSkinOptionsWidget()));
+  }
+}
+
+class ImagePickerRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('SheepSkin Settings'),
+        ),
+        body: Center(child: SheepSkinImagePickerWidget()));
   }
 }
 
 /// This is the stateful widget that the main application instantiates.
-class SheepskinSettingsWidget extends StatefulWidget {
-  const SheepskinSettingsWidget({Key? key}) : super(key: key);
+class SheepSkinOptionsWidget extends StatefulWidget {
+  const SheepSkinOptionsWidget({Key? key}) : super(key: key);
 
   @override
-  State<SheepskinSettingsWidget> createState() =>
-      _SheepskinSettingsWidgetState();
+  State<SheepSkinOptionsWidget> createState() => _SheepSkinOptionsWidgetState();
 }
 
-/// This is the private State class that goes with MyStatefulWidget.
-class _SheepskinSettingsWidgetState extends State<SheepskinSettingsWidget> {
+class SheepSkinImagePickerWidget extends StatefulWidget {
+  const SheepSkinImagePickerWidget({Key? key}) : super(key: key);
+
+  @override
+  State<SheepSkinImagePickerWidget> createState() =>
+      _SheepSkinImagePickerWidgetState();
+}
+
+class _SheepSkinImagePickerWidgetState
+    extends State<SheepSkinImagePickerWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.all(8.0),
+        child: TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('fish')));
+  }
+}
+
+class _SheepSkinOptionsWidgetState extends State<SheepSkinOptionsWidget> {
   List<String> validTimeValues = <String>['1', '5', '10', '100'];
-  List<String> validTimeUnits = <String>['Minutes', 'Hours', 'Days', 'Weeks'];
-  String timeUnit = '5';
-  String timeValue = '?';
+  List<String> validTimeUnits = <String>['minutes', 'hours', 'days', 'weeks'];
+  String timeValue = '1';
+  String timeUnit = 'days';
 
   @override
   Widget build(BuildContext context) {
-    timeValue = validTimeValues.first;
-    timeUnit = validTimeUnits.first;
-
-    var timeValueDropdown = DropdownButton<String>(
-      value: timeValue,
-      icon: const Icon(Icons.arrow_downward),
-      iconSize: 24,
-      elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
-      onChanged: (String? newValue) {
-        setState(() {
-          timeValue = newValue!;
-        });
-      },
-      items: validTimeValues.map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-    );
-
-    var timeUnitDropdown = DropdownButton<String>(
-      value: timeUnit,
-      icon: const Icon(Icons.arrow_downward),
-      iconSize: 24,
-      elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
-      onChanged: (String? newValue) {
-        setState(() {
-          timeUnit = newValue!;
-        });
-      },
-      items: validTimeUnits.map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-    );
-
-    var frequencyLine = Row(children: <Widget>[
-      Text('Changes images every'),
-      timeValueDropdown,
-      timeUnitDropdown
+    var locationRow = Row(children: [
+      Padding(padding: EdgeInsets.all(8.0), child: Text('Pick images from ')),
+      Expanded(
+          flex: 2,
+          child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ImagePickerRoute()),
+                    );
+                  },
+                  child: Text('fish')))),
+    ]);
+    var timeRow = Row(children: [
+      Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text('Change the background every')),
+      Padding(
+          padding: EdgeInsets.all(8.0),
+          child: DropdownButton<String>(
+            value: timeValue,
+            icon: const Icon(Icons.arrow_downward),
+            iconSize: 24,
+            elevation: 16,
+            style: const TextStyle(color: Colors.pink),
+            onChanged: (String? newValue) {
+              setState(() {
+                timeValue = newValue!;
+              });
+            },
+            items:
+                validTimeValues.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          )),
+      Padding(
+          padding: EdgeInsets.all(8.0),
+          child: DropdownButton<String>(
+            value: timeUnit,
+            icon: const Icon(Icons.arrow_downward),
+            iconSize: 24,
+            elevation: 16,
+            style: const TextStyle(color: Colors.deepPurple),
+            underline: Container(
+              height: 2,
+              color: Colors.deepPurpleAccent,
+            ),
+            onChanged: (String? newValue) {
+              setState(() {
+                timeUnit = newValue!;
+              });
+            },
+            items: validTimeUnits.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ))
     ]);
 
-    var spaceCheckbox = Checkbox(
-      onChanged: (bool? newValue) {
-        setState(() {
-          //timeUnit = newValue!;
-        });
-      },
-      value: true,
-    );
-
-    var sourceLine = Row(children: <Widget>[
-      Text('Get images from'),
-      spaceCheckbox,
-      IconButton(
-        icon: const Icon(Icons.volume_up),
-        tooltip: 'Pick locations',
-        onPressed: () {
-          setState(() {
-            // Do a  thing
-          });
-        },
-      ),
-    ]);
-
-    var lines = Column(children: <Widget>[frequencyLine, sourceLine]);
-    return lines;
+    return Column(children: [locationRow, timeRow]);
   }
 }
